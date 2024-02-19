804e3e48c093e53cc13ba67e37ca5fa26470ace8
Christian Niephaus <christian@cnb24.de>
2024-02-19 22:16:21 +0100

    fixed sytax errors

--- sp.tf
+++ sp.tf
@@ -12,22 +12,22 @@ resource "azuread_application" "az_devoops_app_prod" {
   count = var.create_service_principle_prod ? 1 : 0
   display_name = "AzDevOps_${var.devops_project_name}_prod_sp"
   owners       = length(var.service_principle_owner_prod) > 0 ? [data.azuread_user.sp_owner_prod[0].object_id] : []
-  description = "Used by Azure DevOps Project ${devops_project_name} to deploy to prod. Created by TF." 
+  description = "Used by Azure DevOps Project ${var.devops_project_name} to deploy to prod. Created by TF." 
 }
 
 resource "azuread_application" "az_devoops_app_test" {
   count = var.create_service_principle_test ? 1 : 0
   display_name = "AzDevOps_${var.devops_project_name}_test_sp"
   owners       = length(var.service_principle_owner_test) > 0 ? [data.azuread_user.sp_owner_test[0].object_id] : []
-  description = "Used by Azure DevOps Project ${devops_project_name} to deploy to test. Created by TF."
+  description = "Used by Azure DevOps Project ${var.devops_project_name} to deploy to test. Created by TF."
 }
 
 resource "azuread_service_principal" "az_sp_prod" {
   count = var.create_service_principle_prod ? 1 : 0
   client_id               = azuread_application.az_devoops_app_prod[0].client_id
   app_role_assignment_required = false
-  owners       = length(var.service_principle_owner_prod) > 0 ? [data.azuread_user.sp_owner_test[0].object_id : []
-  description = "Used by Azure DevOps Project ${devops_project_name} to deploy to prod. Created by TF." 
+  owners       = length(var.service_principle_owner_prod) > 0 ? [data.azuread_user.sp_owner_test[0].object_id] : []
+  description = "Used by Azure DevOps Project ${var.devops_project_name} to deploy to prod. Created by TF." 
 }
 
 resource "azuread_service_principal" "az_sp_test" {
@@ -35,7 +35,7 @@ resource "azuread_service_principal" "az_sp_test" {
   client_id               = azuread_application.az_devoops_app_test[0].client_id
   app_role_assignment_required = false
   owners       = length(var.service_principle_owner_test) > 0 ? [data.azuread_user.sp_owner_test[0].object_id] : []
-  description = "Used by Azure DevOps Project ${devops_project_name} to deploy to test. Created by TF." 
+  description = "Used by Azure DevOps Project ${var.devops_project_name} to deploy to test. Created by TF." 
 }
 
 resource "azuread_service_principal_password" "az_sp_pwd_prod" {
